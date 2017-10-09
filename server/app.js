6a72f9047cb699d62b5cf9cd97b8a9e1f914c594
ritorrehuelamo <torrehuelamo@gmail.com>
2017-10-09 12:06:07 +0200

    Dotenv config

--- server/app.js
+++ server/app.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const favicon = require('serve-favicon')
-const session    = require('express-session')
+const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const logger = require('morgan')
-const passport   = require('passport')
+const passport = require('passport')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const debug = require('debug')("app:"+path.basename(__filename).split('.')[0])
+const debug = require('debug')("app:" + path.basename(__filename).split('.')[0])
 const mongoose = require('mongoose')
 const cors = require('cors')
 
@@ -16,12 +16,16 @@ const rootRouter = require('./routes/')
 
 const app = express()
 
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 require('./config/dbConfig')
 
 const whiteList = ['http://localhost:4200']
 
 const corsOptions = {
-  origin: function (origin, callback){
+  origin: function (origin, callback) {
     const originIsWhitelisted = whiteList.indexOf(origin) !== -1
     callback(null, originIsWhitelisted)
   },
@@ -35,7 +39,9 @@ app.set('view engine', 'ejs')
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -43,8 +49,13 @@ app.use(session({
   secret: 'LocalMarket',
   resave: true,
   saveUninitialized: true,
-  cookie : { httpOnly: true, maxAge: 60*60*24*365 },
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
+  cookie: {
+    httpOnly: true,
+    maxAge: 60 * 60 * 24 * 365
+  },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
 }))
 
 require('./passport/serializers');
@@ -54,13 +65,13 @@ app.use(passport.session());
 
 app.use('/', rootRouter)
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found')
   err.status = 404
   next(err)
 })
 
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.locals.message = err.message
   res.locals.error = req.app.get('env') === 'development' ? err : {}
   res.status(err.status || 500)
