a866089b52128fa0daaefa881e06d8b86f4ad31c
Ricardo Torre Huélamo <torrehuelamo@gmail.com>
2017-11-17 13:40:17 +0100

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   username: {type: String, required: true},
-  password: {type: String},
+  password: {type: String, required: true},
   firstName: String,
   lastName: String,
   email: String,
